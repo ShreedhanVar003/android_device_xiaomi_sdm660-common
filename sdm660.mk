abaf4f53ea7146ffe074ef450cac465ab1b1979c
JonnyRoller23 <maxim55miui11@gmail.com>
2022-01-27 02:09:17 +0300

    sdm660-common: Added Via Browser Why not ? Signed-off-by: Shreedhan003 <shridhan98@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -144,6 +144,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
+# ViaBrowser
+PRODUCT_PACKAGES += \
+    ViaBrowser
+    
 # Dirac
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.audio.soundfx.dirac=true \
