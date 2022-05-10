94e1eb0c4f053345ab4d5d221721ea2a2f532a8d
Shreedhan003 <shridhan98@gmail.com>
2022-05-10 22:55:03 +0530

    Revert "sdm660-common: Added Via Browser"
    This reverts commit abaf4f53ea7146ffe074ef450cac465ab1b1979c.

--- sdm660.mk
+++ sdm660.mk
@@ -144,10 +144,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 endif
 
-# ViaBrowser
-PRODUCT_PACKAGES += \
-    ViaBrowser
-    
 # Dirac
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.audio.soundfx.dirac=true \
